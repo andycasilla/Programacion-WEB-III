977934f9557a84ad1a87b381699bb5cb117beee7
Andy Casilla <andyjoelcasillamitman@gmail.com>
2026-06-08 16:17:29 -0600

    Delete proyecto directory

--- proyecto/backend/routes/logs.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { registrarLog } from '../controllers/logs.controller.js';
-
-const router = express.Router();
-
-router.post('/', registrarLog);
-
-export default router;
